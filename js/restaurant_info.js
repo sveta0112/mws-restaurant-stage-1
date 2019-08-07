e7c8b7b3245719270d00b61a593cae699db46c04
sveta0112 <sveta0112@hotmail.com>
2019-08-07 16:46:42 -0700

    CSS

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -152,9 +152,11 @@ createReviewHTML = (review) => {
   name.innerHTML = review.name;
   li.appendChild(name);
 
+
   const date = document.createElement('p');
   date.innerHTML = review.date;
   li.appendChild(date);
+
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
